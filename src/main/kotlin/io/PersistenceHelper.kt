d1b109e49de7190e13e426ba0f9bc1a3e6337e52
Jim M. R. Teichgräber <jimmr.teichgraeber@tum.de>
2021-04-23 20:08:44 +0200

    Replaced gson with kotlinx.serialization

--- src/main/kotlin/io/PersistenceHelper.kt
+++ src/main/kotlin/io/PersistenceHelper.kt
@@ -1,76 +1,47 @@
 package io
 
-import com.google.gson.Gson
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import processes.ProcessHandler
 import processes.ProcessHandler.computeReducedProcessList
-import java.io.FileInputStream
-import java.io.FileNotFoundException
 import java.io.IOException
-import java.io.InputStreamReader
 import java.nio.file.Files
 import java.nio.file.Path
 
 object PersistenceHelper {
-    private val DEFAULT_BLACKLIST_SET_PATH = Path.of("res", "blacklisted.json")
-    private val DEFAULT_VOLUME_PATH = Path.of("res", "volume.txt")
-    private val DEFAULT_HIDDEN_PROCESSES_PATH = Path.of("res", "hidden.json")
-    private val gson = Gson()
+    val DEFAULT_BLACKLIST_SET_PATH = Path.of("res", "blacklisted.json")
+    val DEFAULT_VOLUME_PATH = Path.of("res", "volume.txt")
+    val DEFAULT_HIDDEN_PROCESSES_PATH = Path.of("res", "hidden.json")
 
-    @JvmOverloads
-    @Throws(FileNotFoundException::class)
-    fun readBlacklistSet(path: Path = DEFAULT_BLACKLIST_SET_PATH): MutableSet<String> {
-        return gson.fromJson(InputStreamReader(FileInputStream(path.toFile())), mutableSetOf<String>().javaClass)
-    }
-
-    @JvmOverloads
-    @Throws(IOException::class)
-    fun writeBlacklistSet(set: Set<String?>?, path: Path? = DEFAULT_BLACKLIST_SET_PATH) {
-        Files.writeString(path, gson.toJson(set))
-    }
-
-    @JvmOverloads
-    @Throws(IOException::class)
-    fun writeVolume(volume: Double, path: Path? = DEFAULT_VOLUME_PATH) {
-        Files.writeString(path, (volume * 100).toString())
-    }
-
-    @JvmOverloads
-    @Throws(IOException::class)
-    fun writeHiddenProcesses(hiddenProcesses: List<String?>?, path: Path? = DEFAULT_HIDDEN_PROCESSES_PATH) {
-        Files.writeString(path, gson.toJson(hiddenProcesses))
-    }
-
-    @JvmOverloads
-    @Throws(IOException::class)
-    fun readHiddenProcesses(path: Path = DEFAULT_HIDDEN_PROCESSES_PATH): MutableList<String> {
-        return gson.fromJson(InputStreamReader(FileInputStream(path.toFile())), mutableListOf<String>().javaClass)
-    }
-
-    @JvmOverloads
-    @Throws(IOException::class)
     fun startApp() {
-        val blacklisted = readBlacklistSet()
-        ProcessHandler.blacklisted = blacklisted
-        val hiddenProcesses = readHiddenProcesses()
-        ProcessHandler.hiddenProcesses = hiddenProcesses
+        val blacklisted = loadFromFile<MutableSet<String>>(DEFAULT_BLACKLIST_SET_PATH)
+        ProcessHandler.blacklisted = blacklisted ?: mutableSetOf()
+        val hiddenProcesses = loadFromFile<MutableList<String>>(DEFAULT_HIDDEN_PROCESSES_PATH)
+        ProcessHandler.hiddenProcesses = hiddenProcesses ?: ProcessHandler.DEFAULT_HIDDEN_PROCESSES
         computeReducedProcessList(true)
     }
 
-    @JvmOverloads
-    @Throws(IOException::class)
     fun stopApp(volume: Double? = null) {
-        writeBlacklistSet(ProcessHandler.blacklisted)
-        volume?.let { writeVolume(it) }
-        writeHiddenProcesses(ProcessHandler.hiddenProcesses)
+        saveToFile(ProcessHandler.blacklisted, DEFAULT_BLACKLIST_SET_PATH)
+        volume?.let { saveToFile(it, DEFAULT_VOLUME_PATH) }
+        saveToFile(ProcessHandler.hiddenProcesses, DEFAULT_HIDDEN_PROCESSES_PATH)
     }
 
-    @Throws(IOException::class)
-    fun <T> saveObjectToFile(`object`: T, path: Path?) {
-        Files.writeString(path, gson.toJson(`object`))
+    inline fun <reified T> saveToFile(t: T, path: Path) {
+        try{
+            Files.writeString(path,Json.encodeToString(t))
+        }catch (e:IOException){
+
+        }
     }
 
-    @Throws(IOException::class)
-    fun <T> readObjectFromFile(tClass: Class<T>?, path: Path?): T {
-        return gson.fromJson(Files.readString(path), tClass)
+    inline fun <reified T> loadFromFile(path: Path): T? {
+        return try {
+            Json.decodeFromString(Files.readString(path))
+        } catch (e: IOException) {
+            null
+        }
     }
+
 }
